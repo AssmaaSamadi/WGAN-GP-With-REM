05569f343cf5d3f09edd2723d7810e205c7e68c2
AssmaaSamadi <100800521+AssmaaSamadi@users.noreply.github.com>
2024-09-21 06:45:39 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,5 +3,6 @@ from data.preprocessing import
 #load the processor
 processsor = typepreprocee()
 
-#process the data 
+#process the data; this will preprocess the data and split it between training and testing set and will save the sets to the local
+
 processsor.preprocessing()
