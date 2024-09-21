d69568387f6d51e75528d066d14c04a44b91768f
AssmaaSamadi <100800521+AssmaaSamadi@users.noreply.github.com>
2024-09-21 06:44:36 +0200

    Update OptunaWgan.py

--- OptunaWgan.py
+++ OptunaWgan.py
@@ -10,7 +10,9 @@ optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout)
 study_name = "OptunaWgan2"  # Unique identifier of the study.
 storage_name = "sqlite:///{}.db".format(study_name)
 
+# create study to launch it at any time
 study = optuna.create_study(study_name=study_name, direction="maximize",storage=storage_name,load_if_exists=True)
+# launch the study
 study.optimize(objective, n_trials=40)
 
 best_par = study.best_params
@@ -20,6 +22,7 @@ df = study.trials_dataframe().drop(['datetime_start', 'datetime_complete'], axis
 df = df.loc[df['state'] == 'COMPLETE']        # Keep only results that did not prune
 df = df.drop('state', axis=1)                 # Exclude state column
 df = df.sort_values('value')                  # Sort based on accuracy
+
 #save al trials params and results
 df.to_csv('results/GAN/optuna_results2.csv', index=False)  # Save to csv file
 
